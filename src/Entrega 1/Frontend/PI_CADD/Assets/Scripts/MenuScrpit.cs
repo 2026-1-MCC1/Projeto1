2a956d609bdf0302ec3eeb840a758b6cb9c9fa85
caioff2008 <caioff1001@gmail.com>
2026-04-06 08:46:33 -0300

    Criei a pasta script
    Alterei o void start

---
+++ src/Entrega 1/Frontend/PI_CADD/Assets/Scripts/MenuScrpit.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MenuScrpit : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Teste do MENU");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
